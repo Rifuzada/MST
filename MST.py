3c5bb23a19c79a2d2e968194087d48b4901813f6
Rifuzada <84077010+Rifuzada@users.noreply.github.com>
2024-02-12 11:17:12 -0300

    att Riot ID
    att RiotID

--- MST.py
+++ MST.py
@@ -10,8 +10,11 @@ janela.geometry("450x370")
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("blue")
 
+
+
 def open_url(url):
    webbrowser.open_new_tab(url)
+
 
 def region_check():
     b = str(region_entry.get())
@@ -46,97 +49,93 @@ def nick_check():
 
         
 def pegar_id():
-    janela.geometry("450x200")
-    nick = ""
-    x = "%s" % nick_entry.get()
-    nick = x
-    nick.upper()
-    region = ""
-    c = "%s" %region_entry.get()
-    region = c
-    apikey = ""#Sua api key aqui.
-    ids = requests.get(f"https://"+region+".api.riotgames.com/lol/summoner/v4/summoners/by-name/"+nick+"?api_key="+apikey)
-    pf2 = ids.json()
-    summonerid = pf2["id"]
-    mastery = requests.get(f"https://{region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{summonerid}?api_key={apikey}")
-    top_5 = mastery.json()
-    ids = requests.get(f"https://{region}.api.riotgames.com/lol/summoner/v4/summoners/{summonerid}?api_key={apikey}")
-    pf = ids.json()
-    pf1 = str(pf)
+        nick = ""
+        nick = "%s" % nick_entry.get()
+        nick = nick.upper()
+        nick = nick.replace('#', "/")
+        region = ""
+        region = "%s" %region_entry.get()
+        apikey = ""#Sua api key :https://developer.riotgames.com/
+        ids1 = requests.get(f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{nick}?api_key={apikey}")
+        ids1 = ids1.json()
+        puuid = ids1["puuid"]
+        mastery = requests.get(f"https://{region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/{puuid}?api_key={apikey}")
+        top_3 = mastery.json()
 
-    region1 = str(region)
-    a = region1.replace("br1", "BR")
-    a = a.replace("kr", "KR")
-    a = a.replace("tr1","TR")
-    a = a.replace("OC1","OCE")
-    a = a.replace("LA2","LAS")
-    a = a.replace("EUN1","EUNE")
-    a = a.replace("EUW1","EUW")
-    a = a.replace("NA1","NA")
-    a = a.replace("LA1","LAN")
-    a = a.replace("Br1", "BR")
-    l = a.replace("jp1", "JP")
-    
-    def a():
-        janela.geometry('310x300')
-        try:
-            champion_id = top_5[0]["championId"]
-            Maestria1 = top_5[0]["championLevel"]
-            Pontos1 = top_5[0]["championPoints"]
-            Bau1 = top_5[0]["chestGranted"]
-            Maestria2 = top_5[1]["championLevel"]
-            Champion2 = top_5[1]["championId"]
-            Bau2 = top_5[1]["chestGranted"]
-            Pontos2 = top_5[1]["championPoints"]
-            Champion3 = top_5[2]["championId"]
-            Bau3 = top_5[2]["chestGranted"]
-            Pontos3 = top_5[2]["championPoints"]
-            Maestria3 = top_5[2]["championLevel"]
-            if 1 <= int(champion_id) <= 897:
-                Bauz = str(Bau1)
-                a = Bauz.replace("True", "Sim")
-                a = a.replace("False", "Não")
-                Baus = str(Bau2)
-                b = Baus.replace("False", "Não")
-                d = b.replace("True", "Sim")
-                Bau = str(Bau3)
-                e = Bau.replace("False", "Não")
-                f = e.replace("True", "Sim")
-            
-            for text1 in pf.get("name"):
-                text1 = "O perfil pesquisado foi: " + pf["name"] + "\n"
-                text2 =f"A Sua maior maestria é com o champion: {get_champions_name(int(champion_id))}\nSua maestria é de nível {Maestria1}.\nCom {Pontos1:_} pontos de maestria.\nJá ganhou baú com este champion? {a}\nSua Segunda maior maestria é com o champion: {get_champions_name(int(Champion2))}\nSua maestria é de nível {Maestria2}.\nCom {Pontos2:_} pontos de maestria.\nJá ganhou baú com este champion? {d}\nSua Terceira maior maestria é com o champion: {get_champions_name(int(Champion3))}\nSua maestira é de nível {Maestria3}\nCom {Pontos3:_} pontos de maestria.\nJá ganhou baú com este champion? {f}\n"                
-                texto_maestrias["text"] = text2.replace("_", '.')
-                texto_orientacao["text"] = text1
-            button.destroy()
-            info['text']= '' 
-            nick_entry.grid_forget()
-            region_entry.grid_forget()
-        except:
-            texto_maestrias["text"] = "O jogador pesquisado não possui todas as 3 maestrias..."
+        region1 = str(region)
+        a = region1.replace("br1", "BR")
+        a = a.replace("kr", "KR")
+        a = a.replace("tr1","TR")
+        a = a.replace("OC1","OCE")
+        a = a.replace("LA2","LAS")
+        a = a.replace("EUN1","EUNE")
+        a = a.replace("EUW1","EUW")
+        a = a.replace("NA1","NA")
+        a = a.replace("LA1","LAN")
+        a = a.replace("Br1", "BR")
+        l = a.replace("jp1", "JP")
+        
+        def a():
+            janela.geometry('310x300')
+            try:
+                champion_id = top_3[0]["championId"]
+                Maestria1 = top_3[0]["championLevel"]
+                Pontos1 = top_3[0]["championPoints"]
+                Bau1 = top_3[0]["chestGranted"]
+                Maestria2 = top_3[1]["championLevel"]
+                Champion2 = top_3[1]["championId"]
+                Bau2 = top_3[1]["chestGranted"]
+                Pontos2 = top_3[1]["championPoints"]
+                Champion3 = top_3[2]["championId"]
+                Bau3 = top_3[2]["chestGranted"]
+                Pontos3 = top_3[2]["championPoints"]
+                Maestria3 = top_3[2]["championLevel"]
+                if 1 <= int(champion_id) <= 897:
+                    Bauz = str(Bau1)
+                    a = Bauz.replace("True", "Sim")
+                    a = a.replace("False", "Não")
+                    Baus = str(Bau2)
+                    b = Baus.replace("False", "Não")
+                    d = b.replace("True", "Sim")
+                    Bau = str(Bau3)
+                    e = Bau.replace("False", "Não")
+                    f = e.replace("True", "Sim")
+                
+                for text1 in ids1.get("gameName"):
+                    text1 = f"O perfil pesquisado foi: {ids1['gameName']}#{ids1['tagLine']}\n"
+                    text2 =f"A Sua maior maestria é com o champion: {get_champions_name(int(champion_id))}\nSua maestria é de nível {Maestria1}.\nCom {Pontos1:_} pontos de maestria.\nJá ganhou baú com este champion? {a}\nSua Segunda maior maestria é com o champion: {get_champions_name(int(Champion2))}\nSua maestria é de nível {Maestria2}.\nCom {Pontos2:_} pontos de maestria.\nJá ganhou baú com este champion? {d}\nSua Terceira maior maestria é com o champion: {get_champions_name(int(Champion3))}\nSua maestira é de nível {Maestria3}\nCom {Pontos3:_} pontos de maestria.\nJá ganhou baú com este champion? {f}\n"                
+                    texto_maestrias["text"] = text2.replace("_", '.')
+                    texto_orientacao["text"] = text1
+                button.destroy()
+                info['text']= '' 
+                nick_entry.grid_forget()
+                region_entry.grid_forget()
+            except:
+                texto_maestrias["text"] = "O jogador pesquisado não possui todas as 3 maestrias..."
 
 
-    texto_orientacao = Label(janela, text="Clique no botão abaixo para exibir as maestrias de "+ pf["name"], font=("Arial", 10), background="#242424", fg="white") 
-    texto_orientacao.grid(column=0, row=4)
+        texto_orientacao = Label(janela, text=f"Clique no botão abaixo para exibir as maestrias de {ids1['gameName']}#{ids1['tagLine']}", font=("Arial", 10), background="#242424", fg="white") 
+        texto_orientacao.grid(column=0, row=4)
 
-    regiao = Label(janela, text="A Região selecionada é {0}".format(l),bg = "#242424", fg="white")
-    regiao.grid(column=0,row=5)
-    button = customtkinter.CTkButton(master=janela,fg_color="#51087E", text="Exibir", command=a)
-    button.place(relx=0.5, rely=0.5)
-    button.grid(column=0, row=6)
-    
-    region_entry.grid_forget()
-    nick_entry.grid_forget()
-    botao.destroy()
+        regiao = Label(janela, text="A Região selecionada é {0}".format(l),bg = "#242424", fg="white")
+        regiao.grid(column=0,row=5)
+        button = customtkinter.CTkButton(master=janela,fg_color="#51087E", text="Exibir", command=a)
+        button.place(relx=0.5, rely=0.5)
+        button.grid(column=0, row=6)
+        
+        region_entry.grid_forget()
+        nick_entry.grid_forget()
+        botao.destroy()
 
 
-    texto_maestrias = Label(janela, text="",bg = "#242424",  fg="white")
-    texto_maestrias.grid(column=0   , row=7)
-    
+        texto_maestrias = Label(janela, text="",bg = "#242424",  fg="white")
+        texto_maestrias.grid(column=0   , row=7)
+        
+
 
 region_entry = customtkinter.CTkEntry(janela,width=200, height=40, border_width=1, placeholder_text='Ex: br1, na1, kr, tr1, etc...', text_color='silver')
 region_entry.grid(row=6, column=0, padx=10, pady=10)
-nick_entry = customtkinter.CTkEntry(janela, width=430, height=40, border_width=1, placeholder_text="Escreva um nick", text_color="silver")
+nick_entry = customtkinter.CTkEntry(janela, width=430, height=40, border_width=1, placeholder_text="Riot#ID", text_color="silver")
 nick_entry.grid(row=0, column=0, padx=10, pady=10)
 
 botao = customtkinter.CTkButton(master=janela, text="Buscar Maestrias",fg_color="#51087E",  command=lambda: [nick_check(),region_check(), pegar_id()])
@@ -153,3 +152,4 @@ info.grid(column=0, row=25)
 
 
 janela.mainloop()
+
