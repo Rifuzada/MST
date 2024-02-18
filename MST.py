92ad48952bb7283a020bb36297e9d5c67e9d5881
Rifuzada <84077010+Rifuzada@users.noreply.github.com>
2024-02-18 14:57:49 -0300

    simplified some shits on the code
    simplified some shits

--- MST.py
+++ MST.py
@@ -4,85 +4,51 @@ from tkinter import *
 import customtkinter
 import webbrowser
 
-janela = customtkinter.CTk()
-janela.title("Mastery Search Tool - MST, @Rifuzada")
-janela.geometry("448x270")
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("blue")
-
-
-
 def open_url(url):
-   webbrowser.open_new_tab(url)
+    webbrowser.open_new_tab(url)
 
-def openNewWindow():
-    newWindow = customtkinter.CTkToplevel(janela)
-    newWindow.title("caracteres especiais")
-    newWindow.geometry("290x130")
-    newWindow.resizable(False,False)
-    botao1 = customtkinter.CTkButton(master=newWindow,width=4, height=2, text="е",fg_color="#51087E",border_width=1,  command= lambda:set_text("е"))
-    botao1.place(x=68,y=0)
+def open_new_window():
+    new_window = customtkinter.CTkToplevel(janela)
+    new_window.title("caracteres especiais")
+    new_window.geometry("290x130")
+    new_window.resizable(False, False)
+    chars = ["е", "а", "с", "г", "р", "о", "в", "н", "м", "т", "я", "ш", "у", "и", "п", "л", "г", "ф", "д", "ж"]
 
-    botao2 = customtkinter.CTkButton(master=newWindow,width=4, height=2, text="а",fg_color="#51087E",border_width=1 , command= lambda:set_text("а"))
-    botao2.place(x=83,y=0)
+    for i, char in enumerate(chars):
 
-    botao3 = customtkinter.CTkButton(master=newWindow,width=4, height=2, text="с",fg_color="#51087E", border_width=1 , command= lambda:set_text("с"))
-    botao3.place(x=98,y=0)
+        customtkinter.CTkButton(master=new_window, width=4, height=2, text=char, fg_color="#51087E", border_width=1, command=lambda ch=char: set_text(ch)).place(x=68 + 15 * (i % 10), y=25 * (i // 10))
 
-    botao4 = customtkinter.CTkButton(master=newWindow,width=4, height=2, text="г",fg_color="#51087E",border_width=1 ,  command= lambda:set_text("г"))
-    botao4.place(x=114,y=0)
-
-    botao5 = customtkinter.CTkButton(master=newWindow,width=4, height=2, text="р",fg_color="#51087E",border_width=1 ,  command= lambda:set_text("р"))
-    botao5.place(x=125,y=0)
-
-    botao6 = customtkinter.CTkButton(master=newWindow,width=4, height=2, text="о",fg_color="#51087E",border_width=1 ,  command= lambda:set_text("о"))
-    botao6.place(x=140,y=0)
-
-    botao7 = customtkinter.CTkButton(master=newWindow,width=4, height=2, text="в",fg_color="#51087E",border_width=1 ,  command= lambda:set_text("в"))
-    botao7.place(x=155,y=0)
-
-    botao8 = customtkinter.CTkButton(master=newWindow,width=4, height=2, text="н",fg_color="#51087E",border_width=1 ,  command= lambda:set_text("н"))
-    botao8.place(x=170,y=0)
-
-    botao9 = customtkinter.CTkButton(master=newWindow,width=4, height=2, text="м",fg_color="#51087E",border_width=1 ,  command= lambda:set_text("м"))
-    botao9.place(x=185,y=0)
-
-    botao10 = customtkinter.CTkButton(master=newWindow,width=4, height=2, text="т",fg_color="#51087E", border_width=1 , command= lambda:set_text("т"))
-    botao10.place(x=203,y=0)
 
 def region_check():
-    b = str(region_entry.get())
-    if len(b) == 3:
-        pass
-    elif len(b) == 2:
-        pass
-    elif len(b)== 1:
-        print("A região escolhida está incorreta.")
-        info['text']= "A região escolhida está incorreta.\nPor favor coloque a região certa."
-        
-    elif len(b) == 0:
-        info['text'] = "Por favor selecione a região antes de apertar o botao."
+    region = region_entry.get().strip()
+    if len(region) == 0 or len(region) > 3:
+        info1['text'] = "A região escolhida está incorreta.\nPor favor coloque a região certa."
+        region_entry.destroy()
+        nick_entry.destroy()
+        botao.destroy()
+        botaoNovaJanela.destroy()
+    elif len(region) == 1:
+        info1['text'] = "A região escolhida está incorreta.\nPor favor coloque a região certa."
+        region_entry.destroy()
+        nick_entry.destroy()
+        botao.destroy()
+        botaoNovaJanela.destroy()
 
-    elif len(b) > 3:
-        print("A região escolhida está incorreta.")
-        info['text']= "A região escolhida está incorreta.\nPor favor coloque a região certa."
-
-        
 def nick_check():
-    a = str(nick_entry.get())
-    a = a.lower()
-    p = '#'
-    if len(a) <= 16:
-        try:
-            if len(a) <= 1:
-                info['text'] = "Por favor selecione o nick antes de apertar o botao."
-        except:
-            info['text'] = "por favor selecione escreva um nick."
-    elif len(a) > 16:
-        print("O nick escolhido está incorreto.")
-        info['text']= "O nick escolhido está incorreto.\nPor favor coloque o nick certo."
+    nick = nick_entry.get().strip().lower()
+    if len(nick) <= 1:
+        info1['text'] = "Por favor selecione o nick antes de apertar o botao."
+        region_entry.destroy()
+        nick_entry.destroy()
+        botao.destroy()
+        botaoNovaJanela.destroy()
+    elif len(nick) > 16:
+        info1['text'] = "O nick escolhido está incorreto.\nPor favor coloque o nick certo."
+        region_entry.destroy()
+        nick_entry.destroy()
+        botao.destroy()
+        botaoNovaJanela.destroy()
 
-        
 def pegar_id():
         nick = ""
         nick = "%s" % nick_entry.get()#salva a info dada no label nick
@@ -97,103 +63,104 @@ def pegar_id():
         mastery = requests.get(f"https://{region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/{puuid}?api_key={apikey}")#request para puxar maestrias
         top_3 = mastery.json()
 
-        region1 = str(region)
-        a = region1.replace("br1", "BR")
-        a = a.replace("kr", "KR")
-        a = a.replace("tr1","TR")
-        a = a.replace("OC1","OCE")
-        a = a.replace("LA2","LAS")
-        a = a.replace("EUN1","EUNE")
-        a = a.replace("EUW1","EUW")
-        a = a.replace("NA1","NA")
-        a = a.replace("LA1","LAN")
-        a = a.replace("Br1", "BR")
-        l = a.replace("jp1", "JP")
+        region_mapping = {
+            "br1": "BR",
+            "kr": "KR",
+            "tr1": "TR",
+            "OC1": "OCE",
+            "LA2": "LAS",
+            "EUN1": "EUNE",
+            "EUW1": "EUW",
+            "NA1": "NA",
+            "LA1": "LAN",
+            "jp1": "JP"
+        }
+
+        region1 = region
+        for code, abbreviation in region_mapping.items():
+            region1 = region1.replace(code, abbreviation)
         janela.geometry('370x150')
-        def a():
-            janela.geometry('310x300')
+        def display_masteries():
+            janela.geometry('330x190')
             try:
-                champion_id = top_3[0]["championId"]
-                Maestria1 = top_3[0]["championLevel"]
-                Pontos1 = top_3[0]["championPoints"]
-                Bau1 = top_3[0]["chestGranted"]
-                Maestria2 = top_3[1]["championLevel"]
-                Champion2 = top_3[1]["championId"]
-                Bau2 = top_3[1]["chestGranted"]
-                Pontos2 = top_3[1]["championPoints"]
-                Champion3 = top_3[2]["championId"]
-                Bau3 = top_3[2]["chestGranted"]
-                Pontos3 = top_3[2]["championPoints"]
-                Maestria3 = top_3[2]["championLevel"]
-                if 1 <= int(champion_id) <= 897:
-                    Bauz = str(Bau1)
-                    a = Bauz.replace("True", "Sim")
-                    a = a.replace("False", "Não")
-                    Baus = str(Bau2)
-                    b = Baus.replace("False", "Não")
-                    d = b.replace("True", "Sim")
-                    Bau = str(Bau3)
-                    e = Bau.replace("False", "Não")
-                    f = e.replace("True", "Sim")
+                # Get mastery information
+                mastery_info = []
+                for i in range(min(3, len(top_3))):  # Ensure we don't exceed the length of top_3
+                    mastery = top_3[i]
+                    champion_id = mastery["championId"]
+                    level = mastery["championLevel"]
+                    points = mastery["championPoints"]
+                    chest_granted = "Sim" if mastery["chestGranted"] else "Não"
+                    mastery_info.append((champion_id, level, points, chest_granted))
                 
-                for text1 in ids1.get("gameName"):
-                    text1 = f"O perfil pesquisado foi: {ids1['gameName']}#{ids1['tagLine']}\n"
-                    text2 =f"A Sua maior maestria é com o champion: {get_champions_name(int(champion_id))}\nSua maestria é de nível {Maestria1}.\nCom {Pontos1:_} pontos de maestria.\nJá ganhou baú com este champion? {a}\nSua Segunda maior maestria é com o champion: {get_champions_name(int(Champion2))}\nSua maestria é de nível {Maestria2}.\nCom {Pontos2:_} pontos de maestria.\nJá ganhou baú com este champion? {d}\nSua Terceira maior maestria é com o champion: {get_champions_name(int(Champion3))}\nSua maestira é de nível {Maestria3}\nCom {Pontos3:_} pontos de maestria.\nJá ganhou baú com este champion? {f}\n"                
-                    texto_maestrias["text"] = text2.replace("_", '.')
-                    texto_orientacao["text"] = text1
+                # Build text for display
+                text1 = f"O perfil pesquisado foi: {ids1['gameName']}#{ids1['tagLine']}\n"
+                text2 = ""
+                for info in mastery_info:
+                    champion_name = get_champions_name(info[0])
+                    text2 += f"Sua maestria com {champion_name} é de nível {info[1]} com {info[2]:_} pontos.\nJá ganhou baú? {info[3]}\n"
+                texto_maestrias["text"] = text2.replace("_", '.')
+                texto_orientacao["text"] = text1
                 button.destroy()
-                info['text']= '' 
-            except:
+                info1['text'] = ''
+            except (KeyError, IndexError) as e:
                 texto_maestrias["text"] = "O jogador pesquisado não possui todas as 3 maestrias..."
+
+
 
 
         texto_orientacao = Label(janela, text=f"Clique no botão abaixo para exibir as maestrias de {ids1['gameName']}#{ids1['tagLine']}", font=("Arial", 10), background="#242424", fg="white") 
         texto_orientacao.grid(column=0, row=4)
 
-        regiao = Label(janela, text="A Região selecionada é {0}".format(l),bg = "#242424", fg="white")
+        regiao = Label(janela, text="A Região selecionada é {0}".format(region1),bg = "#242424", fg="white")
         regiao.grid(column=0,row=5)
-        button = customtkinter.CTkButton(master=janela,fg_color="#51087E", text="Exibir", command=a)
+        button = customtkinter.CTkButton(master=janela,fg_color="#51087E", text="Exibir", command=display_masteries)
         button.place(relx=0.5, rely=0.5)
         button.grid(column=0, row=6)
+        twitter.place(x=165, y=140)
         
         region_entry.destroy()
         nick_entry.destroy()
         botao.destroy()
-        twitter.destroy()
         botaoNovaJanela.destroy()
 
 
-        texto_maestrias = Label(janela, text="",bg = "#242424",  fg="white")
+        texto_maestrias = Label(janela, text="",bg = "#242424",  fg="white", justify="left")
         texto_maestrias.grid(column=0   , row=7)
-        
 
 def set_text(text):
-    nick_entry.insert(100,text)
-    return
+    nick_entry.insert(END, text)
 
+janela = customtkinter.CTk()
+janela.title("Mastery Search Tool - MST, @Rifuzada")
+janela.geometry("448x270")
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("blue")
+
+janela.bind('<Return>', lambda event: [nick_check(), region_check(), pegar_id()])
 
 # Entry labels
-region_entry = customtkinter.CTkEntry(janela,width=200, height=40, border_width=1, placeholder_text='Ex: br1, na1, kr, tr1, etc...', text_color='silver',justify='center')
-region_entry.grid(row=3, column=0, padx=15, pady=5,columnspan=2, sticky=(W))
-region_entry.place(x=8,y=50)
+region_entry = customtkinter.CTkEntry(janela, width=200, height=40, border_width=1, placeholder_text='Ex: br1, na1, kr, tr1, etc...', text_color='silver', justify='center')
+region_entry.grid(row=3, column=0, padx=15, pady=5, columnspan=2, sticky=(W))
+region_entry.place(x=8, y=50)
 
-nick_entry = customtkinter.CTkEntry(janela, width=430, height=40, border_width=1, placeholder_text="Riot#ID", text_color="silver",justify='center')
-nick_entry.grid(row=0, column=0, padx=10, pady=10,columnspan=3,sticky=(N))
-nick_entry.place(x=8,y=0)
-#Buttons
-botaoNovaJanela = customtkinter.CTkButton(master=janela,width=100, height=20,border_width=3,border_color='#07060d', text="caracteres especiais",fg_color="#51087E",  command= lambda:openNewWindow())
+nick_entry = customtkinter.CTkEntry(janela, width=430, height=40, border_width=1, placeholder_text="Riot#ID", text_color="silver", justify='center')
+nick_entry.grid(row=0, column=0, padx=10, pady=10, columnspan=3, sticky=(N))
+nick_entry.place(x=8, y=0)
+
+# Buttons
+botaoNovaJanela = customtkinter.CTkButton(master=janela, width=100, height=20, border_width=3, border_color='#07060d', text="caracteres especiais", fg_color="#51087E", command=open_new_window)
 botaoNovaJanela.place(x=140, y=120)
 
-botao = customtkinter.CTkButton(master=janela, text="Buscar Maestrias",fg_color="#51087E",border_width=3,border_color='#07060d',  command=lambda: [nick_check(),region_check(), pegar_id()])
-botao.place(x=135,y=150)
+botao = customtkinter.CTkButton(master=janela, text="Buscar Maestrias", fg_color="#51087E", border_width=3, border_color='#07060d', command=lambda: [nick_check(), region_check(), pegar_id()])
+botao.place(x=135, y=150)
 
-url= "https://twitter.com/rifuzada"
-twitter = Label(janela, text="by @Rifuzada",background="#242424", fg="#116530")
-twitter.place(x=165,y=180)
-twitter.bind("<Button-1>", lambda e:open_url(url))
+url = "https://twitter.com/rifuzada"
+twitter = Label(janela, text="by @Rifuzada", background="#242424", fg="#116530")
+twitter.place(x=165, y=180)
+twitter.bind("<Button-1>", lambda e: open_url(url))
 
-info = Label(janela, text="",background="#242424", fg="#116530")
-info.grid(column=0, row=25)
+info1 = Label(janela, text="", background="#242424", fg="#116530")
+info1.grid(column=0, row=25)
 
 janela.mainloop()
-
